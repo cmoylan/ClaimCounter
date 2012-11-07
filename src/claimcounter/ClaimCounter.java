ead1e1a4b4ba01992debe773a41e113fb0e3deeb
Chris Moylan <chris@chrismoylan.com>
2012-11-06 21:29:35 -0600

    refactrin

--- src/claimcounter/ClaimCounter.java
+++ src/claimcounter/ClaimCounter.java
@@ -5,29 +5,14 @@
 package claimcounter;
 
 import java.net.URL;
-import java.util.Calendar;
 import java.util.Random;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-//import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import jfxtras.labs.scene.control.gauge.Gauge;
-import jfxtras.labs.scene.control.gauge.LcdDesign;
-import jfxtras.labs.scene.control.gauge.Radial;
-import jfxtras.labs.scene.control.gauge.SplitFlap;
-import jfxtras.labs.scene.control.gauge.SplitFlapBuilder;
-import jfxtras.labs.scene.control.gauge.StyleModel;
-import jfxtras.labs.scene.control.gauge.StyleModelBuilder;
 //import claimcounter.ClaimCounterController;
 
 /**
@@ -36,10 +21,7 @@ import jfxtras.labs.scene.control.gauge.StyleModelBuilder;
  */
 public class ClaimCounter extends Application {
     private static final Random  RND          = new Random();
-    //private static final long    TIME_PERIOD  = 1000000000l; // about 1 seconds
-    //private static final long    TIME_PERIOD  = 10000000000l; // about 10 seconds
     private static final long    TIME_PERIOD  =   5000000000l; // about 5 seconds
-    private Radial radial1;
     private long                 lastTimeCall = 0;
     private ClaimCounterController controller;
     private ClaimData data;
@@ -68,32 +50,19 @@ public class ClaimCounter extends Application {
         URL location = getClass().getResource("ClaimCounter.fxml");
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(location);
-        Parent root = (Parent) fxmlLoader.load(location.openStream());
+        
+        // Store controller on the instance so that it can be accessed
         controller = fxmlLoader.getController();
-
 
         // Set some window properties
         primaryStage.setResizable(true);
         primaryStage.setTitle("All your claim are belong to us");
+        
+        Parent root = (Parent) fxmlLoader.load(location.openStream());
         primaryStage.setScene(new Scene(root));
 
-
+        // Load data to drive the counter
         data = new ClaimData();
-        //controller.setCounter(dater.getClaimCount());
-        
-        // Create some controls
-        //StyleModel STYLE_MODEL_1 = StyleModelBuilder.create()
-        //                                            .frameDesign(Gauge.FrameDesign.STEEL)
-        //                                            .tickLabelOrientation(Gauge.TicklabelOrientation.HORIZONTAL)
-        //                                            .pointerType(Gauge.PointerType.TYPE14)
-        //                                            .thresholdVisible(true)
-        //                                            .lcdDesign(LcdDesign.STANDARD_GREEN)
-        //                                            .build();
-
-        //radial1 = new Radial(STYLE_MODEL_1);
-        //radial1.setThreshold(30);
-        //radial1.setPrefSize(250, 250);
-        //radial1.setValue(30);
     }
 
     //@Override
@@ -105,11 +74,7 @@ public class ClaimCounter extends Application {
     public void stop() {
         TIMER.stop();
     }
-
-    public double getSampleWidth() { return 600; }
-
-    public double getSampleHeight() { return 600; }
-
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         init(primaryStage);
